011444d7961245f3e208412008a53fbfca0c09f9
Aaroh Mankad <aarohmankad@gmail.com>
2019-06-05 19:12:43 -0700

    move back to old indexing

--- backend/index.py
+++ backend/index.py
@@ -1,24 +1,25 @@
 import json
 from elasticsearch import Elasticsearch
 
-def gen_tweet_data(tweets):
-    for raw_tweet in tweets:
-      tweet = json.loads(raw_tweet)
-      yield {
-        "_index": "bulk_tweets",
-        "_type": "document",
-        "doc": {
-          "username": tweet['user']['name'],
-          "user_image": tweet['user']['profile_image_url'],
-          "user_id": tweet['user']['id'],
-          "time": tweet['created_at'],
-          "text": tweet['text'],
-          "urls": tweet['entities']['urls'],
-          "hashtags": tweet['entities']['hashtags']
-        }
-      }
-
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+es = Elasticsearch([{
+  'host': 'localhost',
+  'port': 9200
+}])
 filename = "portal.json"
+
+i = 1
 with open(filename, 'r') as tweets:
-  bulk(es, gen_tweet_data(tweets))
+  for raw_tweet in tweets:
+    print(i)
+    tweet = json.loads(raw_tweet)
+    tweet_data = {
+      "username": tweet['user']['name'],
+      "user_image": tweet['user']['profile_image_url'],
+      "user_id": tweet['user']['id'],
+      "time": tweet['created_at'],
+      "text": tweet['text'],
+      "urls": tweet['entities']['urls'],
+      "hashtags": tweet['entities']['hashtags']
+    }
+    es.index(index="tweets_test", ignore=400, body=tweet_data, doc_type="document")
+    i += 1
