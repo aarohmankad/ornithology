965095c097119c81bb6ff50c400fb9ff574d568f
natashaorie <norie001@ucr.edu>
2019-06-06 01:49:32 -0700

    removed test marker

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -9,7 +9,6 @@ export const MapContainer = withScriptjs(withGoogleMap((props) =>
     >
         {props.isMarkerShown && <Marker position={{ lat: 33.9806, lng: -117.3755 }} />}
 
-        {props.isMarkerShown && <Marker position={{ lat: 34.0556, lng: -117.1825 }} />}
         {props.tweets.map( (tweet, index) => <Marker key={index} position={{lat: parseFloat(tweet['coordinates'][0]), lng: parseFloat(tweet['coordinates'][1]) }} />)}
 
     </GoogleMap>
